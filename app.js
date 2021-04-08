c035e312b536b7c2bda88416ad167b53f8bc7c2f
dylan-shao <yangshao2014@gmail.com>
2021-04-08 13:52:19 +0800

    reorder folder

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 
 const express = require('express');
 const app = express();
-const client = require('./zooKeeperClient');
-const ZooKeeper = require('zookeeper');
+require('./zkClient');
 
 const port = process.env.PORT;
 
